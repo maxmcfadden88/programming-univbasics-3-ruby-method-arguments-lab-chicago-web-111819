37473c5f8fc24a367dd87b7a9106c81d2861d1bf
Max McFadden <maxmcfadden88@gmail.com>
2019-11-08 20:32:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,9 @@ end
 
 introduction_with_language("Dan", "Ember.js")
 
-
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
